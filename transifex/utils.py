760092c5cd326dbdc6897954f3f6ef0e5e11e675
Eric Fischer <efischer@edx.org>
2018-03-08 16:03:30 -0500

    testing

--- transifex/utils.py
+++ transifex/utils.py
@@ -144,6 +144,8 @@ class Repo:
     def clone(self):
         """Clone the repo."""
         subprocess.run(['git', 'clone', '--depth', '1', self.clone_url], check=True)
+        # branch test - NOMERGE
+        subprocess.run(['git', 'checkout', 'origin/efischer/reactifex'], check=True)
 
     def branch(self):
         """Create and check out a new branch."""
